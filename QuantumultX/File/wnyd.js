4f187084241a60c98ca2654e70e0d9e4de7269c1
Clholic <38876731+Clholic@users.noreply.github.com>
2019-11-23 11:31:59 +0800

    Update wnyd.js

--- QuantumultX/File/wnyd.js
+++ QuantumultX/File/wnyd.js
@@ -10,6 +10,10 @@ http-response ^https?:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json requires-b
 Surge & QX MITM = p.du.163.com
 */
 
+/**
+ * @supported A9DEBB136F7C 8DE20E8149AC ED6FD249DAAC AXXXC005
+ */
+
 var body = $response.body;
 var obj = JSON.parse(body);
 
