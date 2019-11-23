ff4af48c4b4066406caefe4ac5aa7c63ae29fabb
Clholic <38876731+Clholic@users.noreply.github.com>
2019-11-23 11:32:17 +0800

    Update wykaola.js

--- QuantumultX/File/wykaola.js
+++ QuantumultX/File/wykaola.js
@@ -10,6 +10,10 @@ http-response ^https://sp\.kaola\.com/api/openad$ requires-body=1,max-size=0,scr
 Surge & QX MITM = sp.kaola.com
 */
 
+/**
+ * @supported A9DEBB136F7C 8DE20E8149AC ED6FD249DAAC AXXXC005
+ */
+
 var obj = JSON.parse($response.body);
 obj = null;
 $done({body: JSON.stringify(obj)});
