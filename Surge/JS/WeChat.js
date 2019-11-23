e5749242a31f8cdd31f4d169bd9725cd4fd0ea8f
Clholic <38876731+Clholic@users.noreply.github.com>
2019-11-23 11:40:28 +0800

    Update WeChat.js

--- Surge/JS/WeChat.js
+++ Surge/JS/WeChat.js
@@ -6,6 +6,10 @@ http-request ^https://mp\.weixin\.qq\.com/mp/getappmsgad script-path=https://Cho
 hostname = mp.weixin.qq.com
 */
 
+/**
+ * @supported A9DEBB136F7C 8DE20E8149AC ED6FD249DAAC AXXXC005
+ */
+
 var data = {
   body: "{}",
   headers: {
