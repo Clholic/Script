1e1ff1f5ead648125bd399359c8b74d7d4a36960
Clholic <38876731+Clholic@users.noreply.github.com>
2019-11-23 11:41:15 +0800

    Update YouTube.js

--- Surge/JS/YouTube.js
+++ Surge/JS/YouTube.js
@@ -6,6 +6,10 @@ http-request ^https://[\s\S]*\.googlevideo\.com/.*&(oad|ctier) script-path=https
 hostname = *.googlevideo.com
 */
 
+/**
+ * @supported A9DEBB136F7C 8DE20E8149AC ED6FD249DAAC AXXXC005
+ */
+
 var data = {
   body: "{}",
   headers: {
