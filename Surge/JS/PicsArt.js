2ab76723793579b44dc0e28af3ac2772b475a2ff
Clholic <38876731+Clholic@users.noreply.github.com>
2019-11-23 11:38:25 +0800

    Update PicsArt.js

--- Surge/JS/PicsArt.js
+++ Surge/JS/PicsArt.js
@@ -10,6 +10,10 @@ QX1.0.0
 Surge & QX Mitm = api.picsart.c*, api.meiease.c*
 */
 
+/**
+ * @supported A9DEBB136F7C 8DE20E8149AC ED6FD249DAAC AXXXC005
+ */
+
 let obj = JSON.parse($response.body);
 obj.subscription.granted = "true";
 $done({body: JSON.stringify(obj)});
