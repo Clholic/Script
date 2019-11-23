ec5f26274c272cc0c7a4edaeae173da0fb760216
Clholic <38876731+Clholic@users.noreply.github.com>
2019-11-23 11:38:40 +0800

    Update QQNews.js

--- Surge/JS/QQNews.js
+++ Surge/JS/QQNews.js
@@ -6,6 +6,10 @@ http-response https://r\.inews\.qq.com\/get(QQNewsUnreadList|RecommendList) requ
 hostname = r.inews.qq.com
 */
 
+/**
+ * @supported A9DEBB136F7C 8DE20E8149AC ED6FD249DAAC AXXXC005
+ */
+
 var obj = JSON.parse($response.body);
 delete obj.adList;
 $done({body: JSON.stringify(obj)});
