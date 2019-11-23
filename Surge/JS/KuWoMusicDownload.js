390c56179946cd1a63346827eb7d2bba92679596
Clholic <38876731+Clholic@users.noreply.github.com>
2019-11-23 11:36:06 +0800

    Update KuWoMusicDownload.js

--- Surge/JS/KuWoMusicDownload.js
+++ Surge/JS/KuWoMusicDownload.js
@@ -9,5 +9,9 @@ http-response ^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice)
 MITM = vip1.kuwo.cn
 */
 
+/**
+ * @supported A9DEBB136F7C 8DE20E8149AC ED6FD249DAAC AXXXC005
+ */
+
 let url = $request.url.replace(/uid=\d+/g, "uid=1");
 $done({url});
