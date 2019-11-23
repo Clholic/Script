960b6b66aafd13eb84dc085f0569cfbef565b551
Clholic <38876731+Clholic@users.noreply.github.com>
2019-11-23 11:40:15 +0800

    Update VUE.js

--- Surge/JS/VUE.js
+++ Surge/JS/VUE.js
@@ -10,5 +10,9 @@ QX 1.0.0:
 Surge & QX Mitm = api.vuevideo.net
 */
 
+/**
+ * @supported A9DEBB136F7C 8DE20E8149AC ED6FD249DAAC AXXXC005
+ */
+
 body = $response.body.replace(/\"isPremium\":false/, "\"isPremium\":true").replace(/\"valid\":false/, "\"valid\":true")
 $done({body});
