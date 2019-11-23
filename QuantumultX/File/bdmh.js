058315d8acee0c70b395a9e163cc2cb728e5e9e9
Clholic <38876731+Clholic@users.noreply.github.com>
2019-11-23 11:26:37 +0800

    Update bdmh.js

--- QuantumultX/File/bdmh.js
+++ QuantumultX/File/bdmh.js
@@ -8,6 +8,10 @@ Surge4：
 http-response ^https?:\/\/(bd|bdapp|mitaoapp)\.(4008109966|yeduapp)\.(net|com)\/\/index\.php\/api\/User\/userLogin requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/bdmh.js
 */
 
+/**
+ * @supported A9DEBB136F7C 8DE20E8149AC ED6FD249DAAC AXXXC005
+ */
+
 var body = $response.body;
 var obj = JSON.parse(body);
 
