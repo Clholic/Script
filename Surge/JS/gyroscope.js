7f23acdcaea1776c245acbe37b9f308eeda7ca83
Clholic <38876731+Clholic@users.noreply.github.com>
2019-11-23 11:41:31 +0800

    Update gyroscope.js

--- Surge/JS/gyroscope.js
+++ Surge/JS/gyroscope.js
@@ -9,6 +9,10 @@ QuantumultX:
 QX & Surge MITM = api.gyrosco.pe
 */
 
+/**
+ * @supported A9DEBB136F7C 8DE20E8149AC ED6FD249DAAC AXXXC005
+ */
+
 let obj = JSON.parse($response.body);
 obj.user["active_until_time"] = "2099-01-01T00:00:00Z";
 $done({body: JSON.stringify(obj)});
