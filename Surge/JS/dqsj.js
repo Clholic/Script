3a0c522afc784fb63f24dc27bfccaa48c043d543
Clholic <38876731+Clholic@users.noreply.github.com>
2019-11-23 11:41:01 +0800

    Update dqsj.js

--- Surge/JS/dqsj.js
+++ Surge/JS/dqsj.js
@@ -10,6 +10,10 @@ QX：
 Surge & QX MITM = api.mvmtv.com
 */
 
+/**
+ * @supported A9DEBB136F7C 8DE20E8149AC ED6FD249DAAC AXXXC005
+ */
+
 let obj = JSON.parse($response.body);
 let url = $request.url;
 const play = 'vid=';
