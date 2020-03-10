e347a420d1712b1b971dd397b094d9c77a3a307c
Clholic <38876731+Clholic@users.noreply.github.com>
2020-03-10 09:07:10 +0800

    Update unblockremote.js

--- unblockremote.js
+++ unblockremote.js
@@ -1,3 +1,3 @@
 var body = $response.body;
-body = '\/*\n@supported 你的QuantumultX设备ID填这里\n*\/\n' + body;
+body = '\/*\n@supported A9DEBB136F7C 8DE20E8149AC ED6FD249DAAC AXXXC005\n*\/\n' + body;
 $done(body);
