b9a6d7ce7b23f433365049331df4278295075cfd
Clholic <38876731+Clholic@users.noreply.github.com>
2020-03-11 10:55:24 +0800

    Update Splice.js

--- Surge/JS/Splice.js
+++ Surge/JS/Splice.js
@@ -1,11 +1,18 @@
 /*
 Splice app unlock pro.
+
 Surge4.0:
 http-response https:\/\/splice\.oracle\.\w+\.com\/devices\/me requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Splice.js
+
 QX 1.0.0:
 https:\/\/splice\.oracle\.\w+\.com\/devices\/me url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Splice.js
+
 Surge & QX Mitm = splice.oracle.*.com
 */
+
+/**
+ * @supported A9DEBB136F7C 8DE20E8149AC ED6FD249DAAC AXXXC005
+ */
 
 var obj = JSON.parse($response.body); 
 obj['will_renew_subscription'] = true; 
